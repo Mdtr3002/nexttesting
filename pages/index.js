75ff82f576c0d19425c5cc5984efe0d281698c61
Trần Duy Minh <minhduytranct2017@gmail.com>
2022-01-18 19:42:51 +0700

    performance

--- pages/index.js
+++ pages/index.js
@@ -22,6 +22,7 @@ export default function Home( {allPostsData} ) {
            width={144} // Desired size with correct aspect ratio
            alt="Your Name"
            /><br/>
+           <p>{post.name.first}<br/>{post.email}<br/>{post.cell}</p>
            </div>
          ))}
         </div>
